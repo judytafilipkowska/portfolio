0d005193bc998c4d6da397f9efeb919c32e5c3d1
Judyta Filipkowska <judytafilipkowska@gmail.com>
2022-01-26 10:16:53 +0000

    create welcome component

---
+++ src/components/welcome/welocome.js
@@ -0,0 +1,25 @@
+import './welcome.css'
+import Picture from '../../image/—Pngtree—wooden hanging with tropical_3551374.png'
+
+function Welcome() {
+    return (
+        <div className="welcome">
+            <div className="welcome-left">
+                <div className="welcome-left-wrapper">
+                    <h2><div className="welcome-message">Hey, My name is </div></h2>
+                    <h3><div className="welcome-name">Judyta Filipkowska </div></h3>
+                    <p className="welcome-desription">
+                        and I am a Full-Stack Developer, and a wannabe dancer.
+                        Originally from Poland, have lived in Portugal since 2019.
+                    </p>
+                </div>
+
+            </div>
+            <div className="welcome-right">
+                <div className="welcome-background"> </div>
+                <img src={Picture} alt="" className="welcome-picture" />
+            </div>
+        </div>
+    )
+}
+export default Welcome;
