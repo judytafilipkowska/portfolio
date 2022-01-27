b2fc10f252bb5d697bbd90e50d85e56d65f65ace
Judyta Filipkowska <judytafilipkowska@gmail.com>
2022-01-27 14:41:48 +0000

    change styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,7 +15,6 @@ function About() {
             </div>
             <div className="about-right">
                 <h1 className="about-heading">About me</h1>
-                <p className="about-subheading"> Words</p>
                 <p className="about-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, id facilis. Ea sapiente aliquam inventore, aperiam possimus voluptatibus beatae veniam soluta maxime omnis cum reiciendis veritatis, dolores placeat, esse qui.</p>
                 <div className="about-pics">
                     <img src={Palm} alt="" className="about-my-pics" />
