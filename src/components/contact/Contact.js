fc82ab4303bec21483eba14e97523fb5f0009026
Judyta Filipkowska <judytafilipkowska@gmail.com>
2022-01-27 11:01:14 +0000

    add responsive design, corrected ProjectList and Contact components

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,18 +4,33 @@ import GitHub from "../../image/github.png"
 import Mail from "../../image/mail.png"
 import Pin from "../../image/pin.png"
 import Phone from "../../image/phone.png"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import emailjs, { sendForm } from '@emailjs/browser';
 
 function Contact() {
     const formRef = useRef()
+    const [done, setDone] = useState(false);
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userSubject, setUserSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleUserName = (e) => setUserName(e.target.value);
+    const handleUserSubject = (e) => setUserSubject(e.target.value);
+    const handleUserEmail = (e) => setUserEmail(e.target.value);
+    const handleMessage = (e) => setMessage(e.target.value)
 
     const handleSubmit = (e) => {
 
         e.preventDefault();
-        emailjs.sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, formRef.current, process.env.USER_ID)
+        emailjs.sendForm(`${process.env.REACT_APP_SERVICE_ID}`, `${process.env.REACT_APP_TEMPLATE_ID}`, formRef.current, `${process.env.REACT_APP_USER_ID}`)
             .then((result) => {
                 console.log(result.text);
+                setDone(true);
+                setUserName("");
+                setUserEmail("");
+                setUserSubject("");
+                setMessage("");
             }, (error) => {
                 console.log(error.text);
             });
@@ -60,11 +75,12 @@ function Contact() {
                         <b>Get in touch!</b> You can send me a  message here:
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Your name" name="user_name"></input>
-                        <input type="text" placeholder="Subject" name="user_subject"></input>
-                        <input type="text" placeholder="Your email address" name="user_email"></input>
-                        <textarea rows="5" placeholder="Message..." name="message"></textarea>
+                        <input type="text" placeholder="Your name" name="user_name" value={userName} onChange={handleUserName}></input>
+                        <input type="text" placeholder="Subject" name="user_subject" value={userSubject} onChange={handleUserSubject}></input>
+                        <input type="text" placeholder="Your email address" name="user_email" value={userEmail} onChange={handleUserEmail}></input>
+                        <textarea rows="5" placeholder="Message..." name="message" value={message} onChange={handleMessage}></textarea>
                         <button type="submit">Send</button>
+                        {done && <p className="send-message">I will get back to you soon!</p>}
                     </form>
                 </div>
             </div>
